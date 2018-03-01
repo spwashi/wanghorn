00e8d08923eab78f85e2aa3e0000448fcf09a884
Sam Washington <spwashi2@illinois.edu>
2018-02-28 18:50:22 -0600

    updated page

--- app/view/js/app/components/content/page/index.js
+++ app/view/js/app/components/content/page/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import PropTypes from "prop-types";
+import React, {Component} from 'react';
 
-export const PageContent = ({pageTitle, pageClass, children}) => {
-    return (
-        <div className={"page " + pageClass}>
-            <h1>{pageTitle}</h1>
-            {children}
-        </div>
-    )
-};
+export class PageContent extends Component {
+    componentDidMount() {
+        document.title = this.props.pageTitle;
+    }
+    
+    render() {
+        const {pageClass, children} = {pageClass: this.props.pageClass, children: this.props.children};
+        return (
+            <div className={"page " + pageClass.replace('.', '')}>
+                {children}
+            </div>
+        )
+        
+    }
+}
+
+PageContent.propTypes = {
+    pageClass: PropTypes.string,
+    pageTitle: PropTypes.string.isRequired
+}
+;
