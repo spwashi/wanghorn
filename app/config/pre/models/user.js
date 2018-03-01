0a2436f768f9ac18b0ac109b4a9f20b8c0e1838c
Sam Washington <spwashi2@illinois.edu>
2018-03-01 15:00:42 -0600

    commented out unimplemented feature

--- app/config/pre/models/user.js
+++ app/config/pre/models/user.js
@@ -6,13 +6,14 @@ const STRING_  = 'string';
 const INTEGER_ = 'int';
 const NULL_    = 'null';
 
-export const inherits                 = '_';
-export const properties               = {
+export const inherits   = '_';
+export const properties = {
     email:      {length: 255, datatypes: [STRING_], unique: true},
     first_name: {length: 50, datatypes: [STRING_, NULL_]},
     last_name:  {length: 50, datatypes: [STRING_, NULL_]}
 };
-export const relatedTo                = {};
+export const relatedTo  = {};
+/*
 relatedTo[Model.identify('emails')]   =
     [
         {
@@ -81,3 +82,4 @@ relatedTo[Model.identify('accounts')] = {
         }
     }
 };
+*/
