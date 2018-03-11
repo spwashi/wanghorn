dffb0f3ae84b4e443ba2ca3cde5781fab4bf6fc9
Sam Washington <sam.p.washington@gmail.com>
2018-03-11 17:52:53 -0500

    Removed outermost .babelrc file, Made Link more flexible

--- app/view/js/components/navigation/components/link.js
+++ app/view/js/components/navigation/components/link.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import {NavLink} from "react-router-dom"
 
-export const Item      = ({children, to, as}) =>
-    <li className="navigation--link_item link_item">
-        <NavLink to={to} exact activeClassName="active link_item--active">
+export const Link      = ({to, children, exact, activeClassName, className}) =>
+    <NavLink to={to}
+             exact={!!exact}
+             className={className || ''}
+             activeClassName={"active link_item--active " + (activeClassName || '')}>
+        {children}
+    </NavLink>;
+export const Item      = ({activeClassName, to, as, className}) =>
+    <li className={"navigation--link_item link_item " + (className || '')}>
+        <Link to={to} activeClassName={activeClassName}>
             {as}
-        </NavLink>
+        </Link>
     </li>;
-export const Container = ({children}) => <ul className="navigation--link_container link_container">{children}</ul>;
+export const Container = ({children}) =>
+    <ul className="navigation--link_container link_container">
+        {children}
+    </ul>;
