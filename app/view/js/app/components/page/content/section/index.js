ddcc3ba973dfcf2bd02529b73570cb2a86d6acd9
Sam Washington <sam@spwashi.com>
2018-04-05 03:03:44 -0500

    updated front page

--- app/view/js/app/components/page/content/section/index.js
+++ app/view/js/app/components/page/content/section/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import * as PropTypes from "prop-types"
 
-
 const ContentSection = ({name, children}) => {
     return (
         <section id={name}>
-            <a name={name}>
-                {children}
-            </a>
+            <a name={name} />
+            {children}
         </section>
     )
 };
--- app/view/js/app/scenes/home/index.js
+++ app/view/js/app/scenes/home/index.js
@@ -2,14 +2,23 @@ import React from "react"
 import {PageContent} from "../../components/page";
 import {PUBLIC__IMAGES} from "../../../paths";
 import {ProfileImageContainer} from "./profile/image/container";
+import ContentSection from "../../components/page/content/section";
 
 export const Home = () => {
     return (
         <PageContent pageTitle="Home" pageClass=".page--__--home">
-            <ProfileImageContainer>
-                <img src={`${PUBLIC__IMAGES}/` + 'smiley.png'}
-                     alt="Hey there!" />
-            </ProfileImageContainer>
+            <ContentSection name={'profile'}>
+                <ProfileImageContainer>
+                    <img src={`${PUBLIC__IMAGES}/` + 'smiley.png'}
+                         alt="Hey there!" />
+                </ProfileImageContainer>
+                <section className="about--me">
+                    <p>
+                        Hello! My name is William Wanghorn, and I'm a dummy site built by Sam Washington.
+                        My main goal is to make it easy for Sam to quickly build and deploy web sites and applications!
+                    </p>
+                </section>
+            </ContentSection>
         </PageContent>
     );
 };
