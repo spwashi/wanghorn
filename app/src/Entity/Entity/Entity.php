8e4bfe9f8b4c1e1d5bddf529eb34c7ac6dfb99c7
Sam Washington <sam@spwashi.com>
2018-06-27 09:33:00 -0500

    minor code-style tweaks

--- app/src/Entity/Entity/Entity.php
+++ app/src/Entity/Entity/Entity.php
@@ -12,36 +12,34 @@ use Sm\Data\Evaluation\Validation\ValidationResult;
 use Sm\Data\Property\Property;
 
 abstract class Entity extends \Sm\Data\Entity\Entity implements Monitored {
-    
-    #
-    ##  Instantiation/Initialization
-    public function initialize() {
-        return $this;
-    }
-    public function findProperty($property_name): Property {
-        $property = $property_name instanceof Property ? $property_name : $this->properties->{$property_name};
-        $this->fillPropertyValue($property);
-        
-        if (!($property instanceof EntityAsProperty)) return $property;
-        
-        $property->find();
-        
-        return $property;
-    }
-    
-    
-    #
-    ##  Persistence
-    abstract public function create(Context $context);
-    
-    
-    #
-    ##  Validation
-    public function validate(Context $context = null): ?ValidationResult {
-        $property_errors = $this->getPropertyValidationErrors($context);
-        $property_errors = array_filter($property_errors);
-        return new EntityValidationResult(count($property_errors) < 1,
-                                          'entity properties checked',
-                                          $property_errors);
-    }
+
+	#
+	##  Instantiation/Initialization
+	public function initialize() {
+		return $this;
+	}
+	public function findProperty($property_name): Property {
+		$property = $property_name instanceof Property ? $property_name : $this->properties->{$property_name};
+		$this->fillPropertyValue($property);
+
+		if (!($property instanceof EntityAsProperty)) return $property;
+
+		$property->find();
+
+		return $property;
+	}
+
+
+	#
+	##  Persistence
+	abstract public function create(Context $context);
+
+
+	#
+	##  Validation
+	public function validate(Context $context = null): ?ValidationResult {
+		$property_errors = $this->getPropertyValidationErrors($context);
+		$property_errors = array_filter($property_errors);
+		return new EntityValidationResult(count($property_errors) < 1, 'entity properties checked', $property_errors);
+	}
 }
