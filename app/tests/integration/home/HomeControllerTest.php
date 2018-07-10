a85bd758d4083f9731e504fedf2af667d4856835
Sam Washington <sam@spwashi.com>
2018-07-10 15:34:41 -0500

    added test to UserController for password

--- app/tests/integration/home/HomeControllerTest.php
+++ app/tests/integration/home/HomeControllerTest.php
@@ -3,24 +3,27 @@
 
 require_once __DIR__ . '/../app.php';
 class HomeControllerTest extends \PHPUnit\Framework\TestCase {
+	/** @var \Sm\Application\Application */
 	public static $app;
 	#
 	##  Setup
 	public static function setUpBeforeClass() {
 		static::$app = resolveApplication();
 		WANGHORN_TEST__DROP_APP_TABLES(static::$app);
+		WANGHORN_TEST__BUILD_APP_TABLES(static::$app);
 	}
 
 	#
 	## Tests
+
+	# Doesn't actually check the JavaScript, only verifies that no exceptions are thrown in the process
 	public function testCanServeIndex() {
 		$this->addToAssertionCount(1);
+		$session_user = static::$app->controller->get('[User]@findSessionUser')->resolve();
+
+		static::$app->controller->get('[Home]@index')->resolve();
+		var_dump($session_user);
 
 
-		# Doesn't actually check the JavaScript, only verifies that no exceptions are thrown in the process
-		static::$app
-			->controller
-			->get('[Home]@index')
-			->resolve();
 	}
 }
--- app/tests/integration/user/signup/UserControllerTest.php
+++ app/tests/integration/user/signup/UserControllerTest.php
@@ -22,12 +22,29 @@ class UserControllerTest extends \PHPUnit\Framework\TestCase {
 		$this->assertFalse($result__empty->status); # Should be false because we haven't posted anything
 
 
-		# Attempt again with a password that is too short
-		Sm::$globals->post['properties'] = ['username' => 'spiget', 'password' => 'boonboonboon'];
-		$result__tooShort                = $createUser();
+		# Attempt again with a normal password
+		$username = 'spiget_' . \Sm\Core\Util::generateRandomString(5, \Sm\Core\Util::ALPHA);
+		$password = 'boonboonboon';
 
-//echo json_encode($result, JSON_PRETTY_PRINT);
-		var_dump($result__tooShort);
+		Sm::$globals->post['properties'] = ['username' => $username, 'password' => $password];
+		$result__okay                    = $createUser();
+		$modelPersistenceManager         = $this->app->data->models->persistenceManager;
+
+		##  USER
+		$user_model = $this->app->data->models->instantiate('user')->set(['username' => $username]);
+		$user_id    = $modelPersistenceManager->find($user_model)->properties->id->value;
+		$this->assertInternalType('int', $user_id);
+
+		##  PASSWORD
+		$password_model  = $this->app->data->models->instantiate('password')->set(['user_id' => $user_id]);
+		$hashed_password = $modelPersistenceManager->find($password_model)->properties->password->value;
+
+		# # # # should be hashed and pass verification
+		$this->assertNotEquals($hashed_password, $password);
+		$this->assertTrue(password_verify($password, $hashed_password));
+
+		echo json_encode($result__okay, JSON_PRETTY_PRINT);
+//		var_dump($result__okay);
 	}
 	public function testLogin() {
 		Sm::$globals->server['REQUEST_METHOD'] = 'POST';
